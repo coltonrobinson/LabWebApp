6eb0cf33eca1cec58dd6b02dbab483bf0b9106fb
alexyjs <alexyjs1@gmail.com>
2023-11-17 20:23:45 +0200

    Update API header

--- backend/server.js
+++ backend/server.js
@@ -73,6 +73,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Expose-Headers', 'Access-Token');
   next();
 });
 
