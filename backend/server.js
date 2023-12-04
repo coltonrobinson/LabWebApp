264f05e69ffdcef28439454b0ffbe29d32f5db86
Colton <colton@bluemel.me>
2023-12-04 11:15:07 -0700

    Fixing CORS

--- backend/server.js
+++ backend/server.js
@@ -56,10 +56,6 @@ pool.connect((err, client, release) => {
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
-<<<<<<< HEAD
-const port = 443;
-=======
->>>>>>> 36ced255bc5e625c6423206813d0fbc929dd868d
 app.locals.pool = pool;
 app.use(cors({
     origin: '*',
