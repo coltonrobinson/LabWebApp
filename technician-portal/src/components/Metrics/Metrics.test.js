5e932544b52e9b7339b97b9b9efc634ef25cb26b
Colton <colton@bluemel.me>
2023-12-18 16:51:47 -0700

    Moving mock live data to pass actions in github

--- technician-portal/src/components/Metrics/Metrics.test.js
+++ technician-portal/src/components/Metrics/Metrics.test.js
@@ -16,7 +16,7 @@ beforeEach(() => {
             case `http://${ip}/api/get-calibrations-by-month/`:
                 return Promise.resolve({ data: [{ order_id: 10000 }, { order_id: 10001 }] })
             case `http://${ip}/api/get-calibration-procedures/`:
-                return Promise.resolve({ data: [{ calibration_procedure_id: 10001 }] })
+                return Promise.resolve({ data: [{"calibration_procedure_id":1,"calibration_procedure":"Monnit Standard Temperature Calibration, 3-Set Points, Leaded, Submersible"},{"calibration_procedure_id":2,"calibration_procedure":"Monnit Low Temperature Calibration, 3-Set Points, Leaded, Submersible"},{"calibration_procedure_id":3,"calibration_procedure":"Monnit Humidity and Temperature Calibration"},{"calibration_procedure_id":4,"calibration_procedure":"Monnit Temperature Calibration, 3-Set Points, Dry"},{"calibration_procedure_id":5,"calibration_procedure":"Monnit Temperature Calibration in Climate Controlled Chamber"}] })
             default:
                 return Promise.reject()
         }
