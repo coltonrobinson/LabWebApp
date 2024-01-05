2eae6e08af6e4e0608bc98de55850bd1cd1c7951
Colton <colton@bluemel.me>
2024-01-05 09:00:26 -0700

    Trying to fix more cors issues

--- backend/server.js
+++ backend/server.js
@@ -76,6 +76,7 @@ app.use('*', (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Expose-Headers', 'Access-Token');
+    res.status(200);
     next();
 });
 
